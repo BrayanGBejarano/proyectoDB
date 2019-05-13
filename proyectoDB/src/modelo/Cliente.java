648bd5aa91e3fc126a5eafd7d93a733c55f2a046
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2019-05-13 12:13:49 -0500

    MODELO. FALTA ORGANIZAR DATOS

--- proyectoDB/src/modelo/Cliente.java
+++ proyectoDB/src/modelo/Cliente.java
@@ -1,4 +1,4 @@
-package proyectoDB;
+package modelo;
 
 import java.util.Date;
 
---
+++ proyectoDB/src/modelo/ConeccionBD.java
@@ -0,0 +1,28 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConeccionBD {
+	
+	private static  Connection connection;
+	public static Connection getConection() throws Exception {
+		if(connection==null) {
+			connection=conectarYConsultarBD();
+		}
+		return connection;
+	}
+	private static Connection conectarYConsultarBD() throws Exception {
+		String ip="200.3.193.24";
+		String puerto="1522";
+		String insta="ESTUD";
+		String user="P09551_1_10";
+		String password=user;
+		 Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection contection=DriverManager.getConnection("jdbc:oracle:thin:@"+ip+":"+puerto+":"+insta,user,password);
+		return contection;
+	}
+	
+	
+
+}
