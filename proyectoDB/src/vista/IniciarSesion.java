4c85dc3358c095bca2703aefac9a43794d6c2f79
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2019-05-25 15:07:36 -0500

    Ventana iniciar sesion

---
+++ proyectoDB/src/vista/IniciarSesion.java
@@ -0,0 +1,103 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class IniciarSesion extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldUsuario;
+	private JPasswordField passwordFieldContrasena;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IniciarSesion frame = new IniciarSesion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public IniciarSesion() {
+		setTitle("Iniciar Sesion");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 422, 212);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{211, 211, 0};
+		gbl_contentPane.rowHeights = new int[] {55, 42, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Usuario:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Contrase\u00F1a");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 0;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textFieldUsuario = new JTextField();
+		GridBagConstraints gbc_textFieldUsuario = new GridBagConstraints();
+		gbc_textFieldUsuario.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_textFieldUsuario.gridx = 0;
+		gbc_textFieldUsuario.gridy = 1;
+		contentPane.add(textFieldUsuario, gbc_textFieldUsuario);
+		textFieldUsuario.setColumns(10);
+		
+		passwordFieldContrasena = new JPasswordField();
+		GridBagConstraints gbc_passwordFieldContrasena = new GridBagConstraints();
+		gbc_passwordFieldContrasena.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordFieldContrasena.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordFieldContrasena.gridx = 1;
+		gbc_passwordFieldContrasena.gridy = 1;
+		contentPane.add(passwordFieldContrasena, gbc_passwordFieldContrasena);
+		
+		JButton btnIniciar = new JButton("Iniciar Sesion");
+		btnIniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//AQUI ADENTRO VA LO QUE DEBEN HACER CON EL BOTON AL REGISTRARSE: 1. LLAMAR LA CONTROLADORA DE INICIO DE SESION
+			}
+		});
+		GridBagConstraints gbc_btnIniciar = new GridBagConstraints();
+		gbc_btnIniciar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnIniciar.gridx = 0;
+		gbc_btnIniciar.gridy = 2;
+		contentPane.add(btnIniciar, gbc_btnIniciar);
+	}
+
+}
